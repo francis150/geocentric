8f189231bc5db81e4117ac903c49278d0e5dad7e
francis150 <francisdelavictoria150@gmail.com>
2022-06-12 22:18:15 +0800

    js and css files versioning

--- includes/geocentric_core_class.php
+++ includes/geocentric_core_class.php
@@ -12,7 +12,7 @@ if (!class_exists('_geocentric_core')) {
         private $plugin_config_controller;
         private $component_controller;
         private $appsero_controller;
-        private $plugin_version = '2.0.3';
+        private $config_data;
         
         function __construct() {
             $this->plugin_config_controller = new _geocentric_plugin_config();
@@ -23,16 +23,16 @@ if (!class_exists('_geocentric_core')) {
             add_action('admin_head', array($this, 'addto_admin_head'));
             add_action('admin_footer', array($this, 'addto_admin_footer'));
             add_action('init', array($this, 'init_component_shortcodes'));
+
+            $this->config_data =  $this->plugin_config_controller->get_plugin_config_data();
         }
 
         public function add_admin_menu_items() {
-            $conig_data = $this->plugin_config_controller->get_plugin_config_data();
-
             add_menu_page(
-                $conig_data['plugin_name'],
-                $conig_data['plugin_name'],
+                $this->config_data['plugin_name'],
+                $this->config_data['plugin_name'],
                 'manage_options',
-                $conig_data['plugin_slug'],
+                $this->config_data['plugin_slug'],
                 array($this, 'navto_main_admin_page'),
                 $this->plugin_config_controller->get_admin_menu_icon(),
                 120
@@ -63,7 +63,7 @@ if (!class_exists('_geocentric_core')) {
 
         public function addto_admin_footer() {
             // Admin page script file
-            wp_enqueue_script('_geocentric_adminpage_scripts', plugins_url( '../admin/js/main_page.js', __FILE__ ), array(), $this->plugin_version);
+            wp_enqueue_script('_geocentric_adminpage_scripts', plugins_url( '../admin/js/main_page.js', __FILE__ ), array(), $this->config_data['plugin_version']);
             
             // npm-uuid cdn import
             wp_enqueue_script('_geocentric_uuid_cdn_script', 'https://cdn.jsdelivr.net/npm/uuid@latest/dist/umd/uuidv4.min.js');
@@ -73,7 +73,7 @@ if (!class_exists('_geocentric_core')) {
             // WP Thickbox
             add_thickbox();
             // Admin page style file
-            wp_enqueue_style('_geocentric_adminpage_styles', plugins_url( '../admin/styles/main_page.css', __FILE__ ), array(), $this->plugin_version);
+            wp_enqueue_style('_geocentric_adminpage_styles', plugins_url( '../admin/styles/main_page.css', __FILE__ ), array(), $this->config_data['plugin_version']);
             // axios
             wp_enqueue_script('_geocentric_axios_cdn_script', 'https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js');
         }
